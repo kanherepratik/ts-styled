dc0575ee390a8d92324b09222c0e16559d74b98f
prateek kanhere <kanherepratik@gmail.com>
2020-04-17 20:33:34 +0530

    add eslint support and rules for typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+const a = 'a';
+if (a === 'a') {
+    console.log(a);
+}
+
+/**
+ *
+ *
+ * @param {string} name
+ * @returns {string}
+ */
+function getGreeting(name: string): string {
+    return `Hello ${name}!`;
+}
